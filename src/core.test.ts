62e7e0ff859ed57fbd1f61ae24eeeff75aa8733c
quincy chang <quincy0710@hotmail.com>
2021-09-19 15:21:08 +0800

    rename test titles

--- src/core.test.ts
+++ src/core.test.ts
@@ -4,47 +4,58 @@ describe('Multiple claimable sessions flow', () => {
   process.env.MaxClaimableSession = '3';
   const core = new Core(0);
 
-  test('Returning 3 from getMaxClaimableSession', () => {
+  test('Get max claimable session', () => {
     expect(core.getMaxClaimableSession()).toEqual(3);
   });
 
-  test('Investing', () => {
+  test('Invest in the first session', () => {
     core.invest('Steve', 10);
     core.invest('Dave', 40);
     expect(core.investments['Steve']).toEqual(10);
   });
 
-  test('Adding profit', () => {
+  test('Add profit in the first session', () => {
     core.addProfit(50);
     expect(core.getProfit()).toEqual(50);
   });
 
-  test('Next session', () => {
+  test('Get claimable profit in the first session', () => {
+    expect(core.getClaimableProfit('Steve')).toEqual(0);
+  });
+
+  test('Go to the second session', () => {
     core.nextSession();
     expect(core.getCurrentSession()).toEqual(1);
   });
 
-  test('Getting claimable profit', () => {
-    expect(core.getClaimableProfit('Steve')).toEqual(10);
-  });
-
-  test('Investing more', () => {
+  test('Invest in the second session', () => {
     core.invest('Steve', 10);
     expect(core.investments['Steve']).toEqual(20);
   });
 
-  test('Adding profit in the different session', () => {
+  test('Add profit in the second session', () => {
     core.addProfit(10);
     expect(core.getProfit()).toEqual(10);
   });
 
-  test('Getting claimable profit in the third sessoin', () => {
-    core.nextSession();
+  test('Get claimable profit in the second session', () => {
     expect(core.getClaimableProfit('Steve')).toEqual(10);
   });
 
-  test('Getting claimable profit in the third sessoin', () => {
+  test('Go to the third session', () => {
     core.nextSession();
-    expect(core.getClaimableProfit('Steve')).toEqual(10);
+    expect(core.getCurrentSession()).toEqual(2);
+  });
+
+  test('Get claimable profit in the third sessoin', () => {
+    expect(core.getClaimableProfit('Steve')).toEqual(13);
+  });
+
+  test('Claim for the first time should return previous claimables', () => {
+    expect(core.claim('Steve')).toEqual(13);
+  });
+
+  test('Claim for the second time should return 0', () => {
+    expect(core.claim('Steve')).toEqual(0);
   });
 });
--- src/session.test.ts
+++ src/session.test.ts
@@ -7,33 +7,33 @@ describe('The session lifecycle', () => {
     Dave: 20
   };
 
-  test('Adding profit of 50', () => {
+  test('Add profit of 50', () => {
     session.addProfit(50);
     expect(session.profit).toEqual(50);
   });
 
-  test('Claiming throw error before settling', () => {
+  test('Claim before settling should throw error', () => {
     expect(() => {
       session.claim('Steve');
     }).toThrow();
   });
 
-  test('Settling the session', () => {
+  test('Settle the session', () => {
     session.settle(investments);
     expect(session.claimable.Steve).toEqual(16);
   });
 
-  test('Adding profit throw error after settling', () => {
+  test('Add profit after settling should throw error', () => {
     expect(() => {
       session.addProfit(50);
     }).toThrow();
   });
 
-  test('Claiming', () => {
+  test('Claim for the first time', () => {
     expect(session.claim('Steve')).toEqual(16);
   });
 
-  test('Return 0 by second claiming', () => {
+  test('Claim for the second time should return 0', () => {
     expect(session.claim('Steve')).toEqual(0);
   });
 });
